929f4007ecfbf9b242a7a77a6203ee0bdb0061a1
Shardul Upadhyay <ushardul@gmail.com>
2011-08-23 16:26:55 -0400

    Circular layout is now more efficient with its given space and is also now centered properly.
    Also added methods that can be called to get the layout to rotate all of its
    contents to the right or to the left.

--- CircularMenu/LayoutTest.py
+++ CircularMenu/LayoutTest.py
@@ -1,17 +1,45 @@
 import kivy
 from circularlayout import CircularLayout
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.graphics import Rectangle, Color
 from kivy.app import App
 
+class RotateRightButton (Button):
+    def __init__(self, callback, **kwargs):
+        self.addCallback = callback
+        super (Button, self).__init__(**kwargs)
+
 class MyApp (App):
+    
     def build (self):
-        c = CircularLayout ()
-        c.add_widget(Button (text='Test'))
-        c.add_widget(Button (text='Test 2'))
-        c.add_widget(Button (text='Test 3'))
-        c.add_widget(Button (text='Test 4'))
-        c.add_widget(Button (text='Test 5'))
-        return c
+        parent = BoxLayout ()
+        self.btnIndex = 1
+        self.c = BoxLayout ()
+        editlayout = BoxLayout (orientation='vertical', size_hint = (0.2, 1.0))
+        addwidgetbutton = Button (text='Add buttons')
+        addwidgetbutton.bind (on_press=self.add_widget_action)
+        rotateleftbutton = Button (text='Rotate Left')
+        rotateleftbutton.bind (on_press=self.rotate_left_action)
+        rotaterightbutton = Button (text='Rotate right')
+        rotaterightbutton.bind (on_press=self.rotate_right_action)
+        editlayout.add_widget (addwidgetbutton)
+        editlayout.add_widget (rotateleftbutton)
+        editlayout.add_widget (rotaterightbutton)
+        parent.add_widget (editlayout)
+        parent.add_widget (self.c)
+        return parent
+
+    def add_widget_action (self, instance):
+        btnText = 'Button text ' + str(self.btnIndex)
+        self.c.add_widget(Button (text=btnText))
+        self.btnIndex += 1
+
+    def rotate_right_action (self, instance):
+        self.c.do_right_rotation()
+
+    def rotate_left_action (self, instance):
+        self.c.do_left_rotation()
 
 if __name__ == '__main__':
     MyApp().run ()
--- CircularMenu/circularlayout.py
+++ CircularMenu/circularlayout.py
@@ -7,6 +7,7 @@ from kivy.uix.widget import Widget
 class CircularLayout (FloatLayout):
     def __init__(self, **kvargs):
         self._max_widgets = 0
+        self._start_angle = 0
         self._trigger_layout = Clock.create_trigger(self._do_layout, -1)
         super (FloatLayout, self).__init__(**kvargs)
         self.bind (children = self._trigger_layout,
@@ -16,29 +17,52 @@ class CircularLayout (FloatLayout):
                    size = self._trigger_layout)
 
     def _do_layout (self, *largs):
-        print 'HERE'
+        if len(self.children) == 0:
+            return
         max_size = 0
-        if len(self.children) > self._max_widgets:
-            for c in self.children:
-                size = math.sqrt(math.pow (c.size[0], 2) + math.pow(c.size[1], 2))
-                if size > max_size:
-                    max_size = size
-            layout_radius = math.ceil (max_size/math.sqrt(2*(1-math.cos(math.pi/len(self.children)))))
-            print len (self.children)
-            temp_width = temp_height = (layout_radius+max_size)*2
-            temp_center_x = (self.pos[0] + temp_width)/2
-            temp_center_y = (self.pos[1] + temp_height)/2
-            theta = 0
-            theta_increment = 2*math.pi/len(self.children)
-            
-            for c in self.children:
-                c.center_x = temp_center_x + math.cos (theta)*layout_radius
-                c.center_y = temp_center_y + math.sin (theta)*layout_radius
-                theta += theta_increment
-
-            self._max_widgets = len(self.children)
-
+        max_child = None
+        for c in self.children:
+            size = math.sqrt(math.pow (c.size[0], 2) + math.pow(c.size[1], 2))/2
+            if size > max_size:
+                max_size = size
+                max_child = c
         
+        layout_radius = math.ceil (max_size/math.sqrt(2*(1-math.cos(math.pi/len(self.children)))))
+        width_given, height_given = self.size
+
+        width_required = (layout_radius + max_child.size[0])*2
+        height_required = (layout_radius + max_child.size[1])*2
+
+        if width_required > width_given:
+            pass
+        if height_required > height_given:
+            pass
+        
+        temp_center_x = self.pos[0] + layout_radius + max_child.size[0]/2
+        temp_center_y = self.pos[1] + layout_radius + max_child.size[1]/2
+        
+        theta = self._start_angle
+        theta_increment = 2*math.pi/len(self.children)
+        
+        for c in self.children:
+            c.center_x = temp_center_x + math.cos (theta)*layout_radius
+            c.center_y = temp_center_y + math.sin (theta)*layout_radius
+            theta += theta_increment
+
+        self._max_widgets = len(self.children)
+
+    def do_left_rotation (self, steps=1):
+        if len(self.children) == 0:
+            return
+        self._start_angle += 2*math.pi/len(self.children)
+        self._do_layout ()
+
+    def do_right_rotation (self, steps=1):
+        if len(self.children) == 0:
+            return
+        self._start_angle -= 2*math.pi/len(self.children)
+        self._do_layout ()
+
     def add_widget (self, widget, index=0):
         widget.bind (
             size = self._trigger_layout,
