ef11905ead92d11f6f179f738b62be462a7394ae
Shardul Upadhyay <ushardul@gmail.com>
2011-08-25 18:04:06 -0400

    Added a working circular menu that can be dragged left or right to rotate. Swipe on the right most button in the menu to bring out an 'info pane'
    Edited the circularlayout as this suited the project better.
    
    Also includes all the images required for the menu.

--- CircularMenu/circularlayout.py
+++ CircularMenu/circularlayout.py
@@ -20,56 +20,27 @@ class CircularLayout (FloatLayout):
         if len(self.children) == 0:
             return
         self._max_widgets = num_widgets = len(self.children)
-
+        temp_center_x, temp_center_y = self.center
         max_width = max_height = 0
-        max_width_child = max_height_child = None
-        wg, hg = self.size
         children = self.children[:]
         
         for c in children:
             csx, csy = c.size
-            if c.size[0] > max_width:
-                max_width = c.size[0]
-                max_width_child = c
-            if c.size[1] > max_height:
-                max_height = c.size[1]
-                max_height_child = c
+            if csx > max_width:
+                max_width = csx
+            if csy > max_height:
+                max_height = csy
                 
         max_diagonal = math.sqrt (max_width*max_width + max_height*max_height)
-        
         layout_radius = math.ceil (max_diagonal/2/math.sqrt(2*(1-math.cos(math.pi/len(children)))))
-        
-        wr = layout_radius*2 + max_width
-        hr = layout_radius*2 + max_height
-        cf_x = cf_y = 1
-        
-        if wr > wg:
-            cf_x = (wg - 2*layout_radius)/max_width
-            if cf_x < 0.3:
-                cf_x = 0.3
-        if hr > hg:
-            cf_y = (hg - 2*layout_radius)/max_height
-            if cf_y < 0.3:
-                cf_y = 0.3
-            
-        max_width, max_height = cf_x * max_width, cf_y*max_height
-        max_diagonal = math.sqrt(math.pow (cf_x*max_width, 2) + math.pow (cf_y*max_height, 2))
-            
-        layout_radius = math.ceil (max_diagonal/2/math.sqrt(2*(1-math.cos(math.pi/len(children)))))
-        temp_center_x = self.pos[0] + layout_radius + max_width/2
-        temp_center_y = self.pos[1] + layout_radius + max_height/2
+                
         theta = 0
         theta_increment = 2*math.pi/len(self.children)
         i = self._first_widget
 
         while True:
             new_center = temp_center_x + math.cos (theta)*layout_radius, temp_center_y + math.sin (theta)*layout_radius
-            csx, csy = children[i].size
-            if (layout_radius*2 + csx) > max_width:
-                csx = cf_x*csx
-            if (layout_radius*2 + csy) > max_height:
-                csy = cf_y*csy
-            self.reposition_child (children[i], center=new_center, size=(csx, csy))
+            self.reposition_child (children[i], center=new_center)
             theta += theta_increment
             i = (i - 1)%num_widgets
             if i == self._first_widget:
