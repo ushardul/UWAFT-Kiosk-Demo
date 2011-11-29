534eb74545ba98a13e9d8b59550766c0ab8e5300
Geo <geossj5@hotmail.com>
2011-11-28 23:14:05 -0500

    some updates

--- Pan/pan.py
+++ Pan/pan.py
@@ -8,6 +8,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.graphics.transformation import Matrix
 
 """
 UI is designed with the kivy language file
