5c49a094d8e4bc454bf9ba2ad3152fb1bfdbde27
Shardul Upadhyay <ushardul@gmail.com>
2012-01-12 12:11:26 -0500

    Fixed bugs and more features.

--- tabcontrol.py
+++ tabcontrol.py
@@ -3,7 +3,6 @@ kivy.require('1.0.9')
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty, StringProperty
 from kivy.config import Config
 from tablayout import TabLayout
@@ -12,16 +11,17 @@ from kivy.uix.button import Button
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.label import Label
 from kivy.uix.image import Image
+from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import *
 
 class InformationView(BoxLayout):
     tab_cont = ObjectProperty(None)
     title = StringProperty ('Title')
     pic_cont = ObjectProperty (None)
-    vid_src = StringProperty ('')
     vid_cont = ObjectProperty (None)
 
     def __init__ (self, title, tabs, video = None, pictures = [], **kvargs):
-        super (BoxLayout, self).__init__(**kvargs)
+        super (BoxLayout, self).__init__(**kvargs)        
         self.title = title
 
         t_lyt = TabLayout ()
@@ -32,21 +32,53 @@ class InformationView(BoxLayout):
         if video is None:
             self.vid_cont.parent.remove_widget (self.vid_cont)
         else:
-            self.vid_src = video
+            self.vid_cont.source = video
+            self.vid_cont.on_touch_down = self._click_vid
         if len (pictures) == 0:
             self.pic_cont.parent.remove_widget (self.pic_cont)
         else:
             for picture in pictures:
                 self.pic_cont.add_widget (picture)
-                picture.bind (height=self.testchange)
+                picture.bind (on_touch_down = self._add_expanded_image)
+                picture.bind (height=self._size_picture)
 
-    def testchange (self, wid, value):
+    def _size_picture (self, wid, value):
         wid.width = wid.image_ratio * value
+
+    def _add_expanded_image (self, wid, touch):
+        print touch
+        for children in self.parent.children:
+            if type (children) == FloatLayout:
+                return
+        self.pre_cont = FloatLayout (size_hint = (1, 1))
+        self.pre_cont.canvas.insert (0, Color (0,0,0,0.5))
+        self.pre_cont.canvas.insert (1, Rectangle (size=self.size))
+        _image = Image (source = wid.source)
+        _btn = Button (text='Dismiss', pos=(15, 15), size_hint= (0.2, 0.1))
+        _btn.bind (on_release =self._rmv_expanded_image)
+        self.pre_cont.add_widget (_image)
+        self.pre_cont.add_widget (_btn)
+        self.parent.add_widget (self.pre_cont)
+        print self.parent.children
+
+    def _rmv_expanded_image (self, touch):
+        print 'Here'
+        if self.pre_cont.parent is not None:
+            print 'Removed'
+            self.pre_cont.parent.remove_widget (self.pre_cont)
+
+    def _click_vid (self, touch):
+        if self.vid_cont.collide_point (touch.x, touch.y):
+            self.vid_cont.play = self.vid_cont.play == False
 
 class InformationApp(App):
     def build (self):
         p = Parser ('C:\Users\Shardul\Desktop\UWAFT-Kiosk-Demo\info.uwaft')
-        return p.get_view ('Some Other View')
+        add = p.get_view ('Some Other View')
+        top = FloatLayout (size_hint=(1, 1))
+        top.add_widget (add)
+        return top
+        
 
 class Parser:
     def __init__ (self, parseSource):
