cc722b889d4172dc01eb9b45fe0c5722cf9f1fdc
Geo <geossj5@hotmail.com>
2012-01-28 22:39:04 -0500

    update

--- test.py
+++ test.py
@@ -4,15 +4,20 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 
-class BoxLay(Widget):
-	pass
+
+class BoxLay(FloatLayout):
+	title_box = ObjectProperty(None)
+	def printStuff(self):
+		print self.title_box.pos
 
 class BoxApp (App):
 	def build (self):
-		return BoxLay()
-
-
+		parent = BoxLay()
+		parent.printStuff()
+		return parent
 
 BoxApp().run()
